7b7903f1341d5eb675e78f90cae81e2d3707276e
ermakov056-cloud <ermakov056@icloud.com>
2026-07-24 22:25:46 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -14,28 +14,68 @@ function createMatrix() {
     return Array.from({ length: SIZE }, () => Array(SIZE).fill(0));
 }
 
-function drawBoard(board, matrix, editable) {
+function drawBoard(board, matrix, editable, enemy = false) {
+
     board.innerHTML = "";
 
     for (let y = 0; y < SIZE; y++) {
+
         for (let x = 0; x < SIZE; x++) {
+
             const cell = document.createElement("div");
             cell.className = "cell";
 
-            if (matrix[y][x] === 1) {
+            if (!enemy && matrix[y][x] == 1)
                 cell.classList.add("ship");
-            }
 
             if (editable) {
-                cell.addEventListener("click", function() {
-                    matrix[y][x] = matrix[y][x] === 1 ? 0 : 1;
-                    drawBoard(board, matrix, editable);
-                });
+
+                cell.onclick = function () {
+
+                    if (gameStarted) return;
+
+                    matrix[y][x] = matrix[y][x] ? 0 : 1;
+
+                    drawBoard(board, matrix, editable, enemy);
+
+                };
+
+            }
+
+            if (enemy) {
+
+                cell.onclick = function () {
+
+                    if (!gameStarted) return;
+
+                    if (cell.classList.contains("hit") ||
+                        cell.classList.contains("miss"))
+                        return;
+
+                    if (matrix[y][x] == 1) {
+
+                        cell.classList.add("hit");
+
+                        statusText.innerHTML = "💥 Попадание!";
+
+                    } else {
+
+                        cell.classList.add("miss");
+
+                        statusText.innerHTML = "🌊 Мимо.";
+
+                    }
+
+                };
+
             }
 
             board.appendChild(cell);
+
         }
+
     }
+
 }
 
 function autoPlaceShips(matrix) {
@@ -81,5 +121,12 @@ autoPlaceBtn.addEventListener("click", function() {
 
     statusText.textContent = "Корабли расставлены";
 });
+document.getElementById("startGame").onclick = function () {
+
+    gameStarted = true;
+
+    statusText.innerHTML = "Ваш ход! Стреляйте по полю противника.";
+
+}
 
 newGame();
