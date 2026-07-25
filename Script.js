6381f6115f28d2f0f1bcb7da21671be00341dae9
ermakov056-cloud <ermakov056@icloud.com>
2026-07-25 06:37:46 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -232,3 +232,233 @@ function showMessage(text) {
     }
 
 }
+/* ===== SCRIPT.JS ===== */
+/* ЧАСТЬ 2 */
+/* Расстановка кораблей */
+
+
+/*
+Размещение всех кораблей
+*/
+
+function placeAllShips(board) {
+
+
+    SHIPS.forEach(ship => {
+
+
+        let placed = false;
+
+
+        while (!placed) {
+
+
+            let x = Math.floor(
+                Math.random() * BOARD_SIZE
+            );
+
+
+            let y = Math.floor(
+                Math.random() * BOARD_SIZE
+            );
+
+
+            let horizontal =
+                Math.random() > 0.5;
+
+
+
+            if (
+                canPlaceShip(
+                    board,
+                    x,
+                    y,
+                    ship.size,
+                    horizontal
+                )
+            ) {
+
+
+                placeShip(
+                    board,
+                    x,
+                    y,
+                    ship.size,
+                    horizontal
+                );
+
+
+                placed = true;
+
+
+            }
+
+
+        }
+
+
+    });
+
+
+}
+
+
+
+/*
+Проверка возможности поставить корабль
+*/
+
+function canPlaceShip(
+    board,
+    x,
+    y,
+    size,
+    horizontal
+) {
+
+
+    for (let i = 0; i < size; i++) {
+
+
+        let nx =
+        horizontal ? x + i : x;
+
+
+        let ny =
+        horizontal ? y : y + i;
+
+
+
+        // Проверка границ
+
+        if (
+            nx < 0 ||
+            ny < 0 ||
+            nx >= BOARD_SIZE ||
+            ny >= BOARD_SIZE
+        ) {
+
+            return false;
+
+        }
+
+
+
+        // Проверка клетки и соседей
+
+        for (
+            let yy = ny - 1;
+            yy <= ny + 1;
+            yy++
+        ) {
+
+
+            for (
+                let xx = nx - 1;
+                xx <= nx + 1;
+                xx++
+            ) {
+
+
+                if (
+                    yy >= 0 &&
+                    xx >= 0 &&
+                    yy < BOARD_SIZE &&
+                    xx < BOARD_SIZE
+                ) {
+
+
+                    if (
+                        board[yy][xx].ship
+                    ) {
+
+                        return false;
+
+                    }
+
+
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+    return true;
+
+}
+
+
+
+/*
+Установка корабля на поле
+*/
+
+function placeShip(
+    board,
+    x,
+    y,
+    size,
+    horizontal
+) {
+
+
+    for (let i = 0; i < size; i++) {
+
+
+        let nx =
+        horizontal ? x + i : x;
+
+
+        let ny =
+        horizontal ? y : y + i;
+
+
+
+        board[ny][nx].ship = true;
+
+
+    }
+
+
+}
+
+
+
+/*
+Проверка поражения кораблей
+*/
+
+function checkWin(board) {
+
+
+    for (let y = 0; y < BOARD_SIZE; y++) {
+
+
+        for (let x = 0; x < BOARD_SIZE; x++) {
+
+
+            if (
+                board[y][x].ship &&
+                !board[y][x].hit
+            ) {
+
+                return false;
+
+            }
+
+
+        }
+
+
+    }
+
+
+    return true;
+
+}
